9f9dc44902a5918add8b62376a4825333d656a26
MahaSayed <41696038+MahaSayed@users.noreply.github.com>
2019-05-08 04:01:16 +0200

    implementation of student settings

---
+++ Graduation Tracking Project/Graduation Tracking Project/Settings.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace Graduation_Tracking_Project
+{
+    partial class Settings
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.EmailtextBox1 = new System.Windows.Forms.TextBox();
+            this.PasswordtextBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button3
+            // 
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Perpetua Titling MT", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(263, 136);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(209, 59);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "Change Password";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Perpetua Titling MT", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(263, 369);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(209, 59);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "Delete Account";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // EmailtextBox1
+            // 
+            this.EmailtextBox1.Location = new System.Drawing.Point(69, 79);
+            this.EmailtextBox1.Name = "EmailtextBox1";
+            this.EmailtextBox1.Size = new System.Drawing.Size(100, 22);
+            this.EmailtextBox1.TabIndex = 15;
+            // 
+            // PasswordtextBox2
+            // 
+            this.PasswordtextBox2.Location = new System.Drawing.Point(69, 194);
+            this.PasswordtextBox2.Name = "PasswordtextBox2";
+            this.PasswordtextBox2.Size = new System.Drawing.Size(100, 22);
+            this.PasswordtextBox2.TabIndex = 16;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(66, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 17);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "Email";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(66, 158);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 17);
+            this.label2.TabIndex = 18;
+            this.label2.Text = "New Password";
+            // 
+            // Settings
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(523, 452);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.PasswordtextBox2);
+            this.Controls.Add(this.EmailtextBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button3);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Settings";
+            this.Text = "Settings";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox EmailtextBox1;
+        private System.Windows.Forms.TextBox PasswordtextBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
---
+++ Graduation Tracking Project/Graduation Tracking Project/Settings.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Graduation_Tracking_Project
+{
+    public partial class Settings : Form
+    {
+        public Settings()
+        {
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string email = Login.logged_user;
+            Student stud = new Student();
+            using (Graduation_Project_TrackerEntitiess ent = new Graduation_Project_TrackerEntitiess())
+            {
+                if (email == EmailtextBox1.Text)
+                {
+                    stud = ent.Students.Where(x => x.student_email == email).FirstOrDefault();
+                    stud.student_password = PasswordtextBox2.Text;
+                    ent.Entry(stud).State = System.Data.Entity.EntityState.Modified;
+                    ent.SaveChanges();
+                    MessageBox.Show("Your Password is updated!");
+
+                }
+
+
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string email = Login.logged_user;
+            Student stud = new Student();
+            using (Graduation_Project_TrackerEntitiess ent = new Graduation_Project_TrackerEntitiess())
+            {
+              
+                stud = ent.Students.Where(x => x.student_email == email).FirstOrDefault();
+                ent.Students.Remove(stud);
+                ent.SaveChanges();
+               
+                MessageBox.Show("Deleted Successfully!");
+                Sign_Up s = new Sign_Up();
+                this.Hide();
+                s.Show();
+            }
+        }
+    }
+}
