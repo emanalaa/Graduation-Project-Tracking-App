b26b01bc11e7a6ba0fbe2ebb2f5b1f05557c36e4
mennaawad <mennamawad16@gmail.com>
2019-05-08 06:24:46 +0200

    update grades
    update grades form

--- Graduation Tracking Project/Graduation Tracking Project/Supervisor and Mentor/Upload Student Grades.Designer.cs
+++ Graduation Tracking Project/Graduation Tracking Project/Supervisor and Mentor/Upload Student Grades.Designer.cs
@@ -28,11 +28,85 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBox_id = new System.Windows.Forms.TextBox();
+            this.textBox_grade = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.update_button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox_id
+            // 
+            this.textBox_id.Location = new System.Drawing.Point(186, 82);
+            this.textBox_id.Multiline = true;
+            this.textBox_id.Name = "textBox_id";
+            this.textBox_id.Size = new System.Drawing.Size(234, 58);
+            this.textBox_id.TabIndex = 1;
+            // 
+            // textBox_grade
+            // 
+            this.textBox_grade.Location = new System.Drawing.Point(186, 205);
+            this.textBox_grade.Multiline = true;
+            this.textBox_grade.Name = "textBox_grade";
+            this.textBox_grade.Size = new System.Drawing.Size(234, 58);
+            this.textBox_grade.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe Print", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(51, 226);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 37);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Grade";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe Print", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(37, 94);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(107, 37);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Team ID";
+            // 
+            // update_button
+            // 
+            this.update_button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.update_button.Font = new System.Drawing.Font("Perpetua Titling MT", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.update_button.Location = new System.Drawing.Point(175, 337);
+            this.update_button.Name = "update_button";
+            this.update_button.Size = new System.Drawing.Size(157, 41);
+            this.update_button.TabIndex = 6;
+            this.update_button.Text = "UPDATE";
+            this.update_button.UseVisualStyleBackColor = true;
+            this.update_button.Click += new System.EventHandler(this.update_button_Click);
+            // 
+            // Upload_Student_Grades
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(532, 440);
+            this.Controls.Add(this.update_button);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_grade);
+            this.Controls.Add(this.textBox_id);
+            this.Name = "Upload_Student_Grades";
             this.Text = "Upload_Student_Grades";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox_id;
+        private System.Windows.Forms.TextBox textBox_grade;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button update_button;
     }
 }
--- Graduation Tracking Project/Graduation Tracking Project/Supervisor and Mentor/Upload Student Grades.cs
+++ Graduation Tracking Project/Graduation Tracking Project/Supervisor and Mentor/Upload Student Grades.cs
@@ -12,9 +12,33 @@ namespace Graduation_Tracking_Project.Supervisor_Mentor_Home
 {
     public partial class Upload_Student_Grades : Form
     {
+        Grade db = new Grade();
         public Upload_Student_Grades()
         {
             InitializeComponent();
+        }
+
+        private void update_button_Click(object sender, EventArgs e)
+        {
+            if (textBox_grade.Text == " " || textBox_id.Text == "")
+            {
+                MessageBox.Show("Please Fill All Informations");
+            }
+            else
+            {
+                Team temp = new Team();
+                temp = db.Teams.Find(textBox_id.Text);
+                if(temp.Team_ID == int.Parse(textBox_id.Text))
+                {
+                    temp.Total_mark = int.Parse(textBox_grade.Text);
+                    db.SaveChanges();
+                    MessageBox.Show("Grades Updated");
+                }
+                else
+                {
+                    MessageBox.Show("No Team Found Please Check The Team ID");
+                }
+            }
         }
     }
 }
