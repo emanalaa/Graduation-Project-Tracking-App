3805d609a98b0c4131ad40e5850abcaa3cc1e083
mennaawad <mennamawad16@gmail.com>
2019-05-08 04:57:08 +0200

    Implemented Post office hours and Identify Task status.

---
+++ Graduation Tracking Project/Graduation Tracking Project/Supervisor and Mentor/Post Office Hours.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace Graduation_Tracking_Project.Supervisor_Mentor_Home
+{
+    partial class Post_Office_Hours
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.update_button = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(283, 160);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(186, 57);
+            this.textBox1.TabIndex = 0;
+            // 
+            // update_button
+            // 
+            this.update_button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.update_button.Font = new System.Drawing.Font("Perpetua Titling MT", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.update_button.Location = new System.Drawing.Point(175, 289);
+            this.update_button.Name = "update_button";
+            this.update_button.Size = new System.Drawing.Size(157, 41);
+            this.update_button.TabIndex = 2;
+            this.update_button.Text = "UPDATE";
+            this.update_button.UseVisualStyleBackColor = true;
+            this.update_button.Click += new System.EventHandler(this.update_button_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe Print", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(22, 170);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(228, 37);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Recent Office Hours";
+            // 
+            // Post_Office_Hours
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(532, 440);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.update_button);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Post_Office_Hours";
+            this.Text = "Post_Office_Hours";
+            this.Load += new System.EventHandler(this.Post_Office_Hours_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button update_button;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ Graduation Tracking Project/Graduation Tracking Project/Supervisor and Mentor/Post Office Hours.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Graduation_Tracking_Project.Supervisor_Mentor_Home
+{
+    public partial class Post_Office_Hours : Form
+    {
+        
+        public Post_Office_Hours()
+        {
+            InitializeComponent();
+        }
+
+        private void Post_Office_Hours_Load(object sender, EventArgs e)
+        {
+            Office_Hourse OF = new Office_Hourse();
+
+            if (Login.supervisor == true)
+            {
+                var query = from sv in OF.Supervisors where sv.supervisor_email == Login.logged_user select sv;
+                Supervisor res = query.Single();
+                textBox1.Text = res.supervisor_office_hours.ToString();
+            }
+            else if(Login.mentor == true)
+            {
+                var query = from mntr in OF.Mentors where mntr.mentor_email == Login.logged_user select mntr;
+                Mentor res = query.Single();
+                textBox1.Text = res.mentor_office_hours.ToString();
+            }
+        }
+
+        private void label_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void update_button_Click(object sender, EventArgs e)
+        {
+             if (textBox1.Text == "")
+            {
+                MessageBox.Show("you hould ")
+            }
+        }
+    }
+}
